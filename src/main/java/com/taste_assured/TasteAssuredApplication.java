85e81543dddd3a1c94ab64696ac4c1df157bb965
Tal_Zuzut <talzuzut3@gmail.com>
2023-10-18 22:14:26 +0300

    [General] Feature: docker-compose, project documentation

---
+++ src/main/java/com/taste_assured/TasteAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.taste_assured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteAssuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taste_assured/TasteAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taste_assured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
